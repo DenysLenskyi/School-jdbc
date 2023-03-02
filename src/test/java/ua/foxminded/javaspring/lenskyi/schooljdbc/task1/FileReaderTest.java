8bd64c27b1362d4ec11b58e15dd0873575d538d3
Denys Lenskyi <denislensky94@gmail.com>
2023-03-02 18:59:53 +0100

    tests deleted. Will add with next commit

--- src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task1/FileReaderTest.java
+++ src/test/java/ua/foxminded/javaspring/lenskyi/schooljdbc/task1/FileReaderTest.java
@@ -6,11 +6,5 @@ import org.junit.jupiter.api.Test;
 import ua.foxminded.javaspring.lenskyi.schooljdbc.task1.domain.FileReader;
 
 public class FileReaderTest {
-    @Test
-    void fileReaderPlainTest() {
-        FileReader reader = new FileReader();
-        assertEquals("CREATE TABLE IF NOT EXISTS school.courses " +
-                        "(ID INT PRIMARY KEY, NAME TEXT, DESCRIPTION TEXT);",
-                reader.readFile("/initiate-table-courses.sql"));
-    }
+
 }
